5a8e585f1e1160a71e28f3c4050c56a3f7a9b738
CUB3D <callumthom11@gmail.com>
2017-08-14 17:29:56 +0100

    PEP8 compliant and renamed constants

--- mobafire.py
+++ mobafire.py
@@ -2,19 +2,23 @@ import sys
 import urllib.request
 from html.parser import HTMLParser
 
-BASE_URL_GUIDE_LIST_PREFIX = "http://www.mobafire.com/league-of-legends/"
+BASE_URL = "http://www.mobafire.com"
+BASE_URL_GAME_LOL = BASE_URL + "/league-of-legends/"
 BASE_URL_GUIDE_LIST_POSTFIX = "-guide"
-BASE_URL_SPECIFIC_GUIDE = "http://www.mobafire.com"
+
 
 def get_champ_guide_url(champion):
-    return BASE_URL_GUIDE_LIST_PREFIX + champion + BASE_URL_GUIDE_LIST_POSTFIX
+    return BASE_URL_GAME_LOL + champion + BASE_URL_GUIDE_LIST_POSTFIX
+
 
 def get_specific_guide_url(url):
-    return BASE_URL_SPECIFIC_GUIDE + url
+    return BASE_URL + url
+
 
 def get_page_html(url):
     data = "".join([chr(x) for x in urllib.request.urlopen(url).read()])
     return data
+
 
 def get_attribute(attributes, name, default=""):
     for attrName, value in attributes:
@@ -22,14 +26,17 @@ def get_attribute(attributes, name, default=""):
             return value
     return default
 
+
 class mobafireParser(HTMLParser):
     guides = []
+
     def handle_starttag(self, tag, attrs):
         if tag == "a":
             className = get_attribute(attrs, name="class")
             href = get_attribute(attrs, name="href")
             if "build-title" in className:
                 self.guides.append(href)
+
 
 def get_guides(championName):
     guideURL = get_champ_guide_url(championName)
@@ -38,8 +45,10 @@ def get_guides(championName):
     parser.feed(get_page_html(guideURL))
     return [get_specific_guide_url(x) for x in parser.guides]
 
+
 def get_top_guide(championName):
     return get_guides(championName)[0]
+
 
 if __name__ == "__main__":
     championName = sys.argv[-1]
